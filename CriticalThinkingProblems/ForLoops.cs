963d5d2338ed15d4d13549aa8b2b7ea00aa98103
telvinmathews <57323861+telvinmathews@users.noreply.github.com>
2020-04-24 10:25:35 -0500

    create a for loop class, created forloop methods that loops 5 time, 9 times backwards, loop based on string, loop that prints each char in string, loop that prints every other char in string, loop that loops basesd on user input, loop that prints every char at index divisible by 3.

---
+++ CriticalThinkingProblems/ForLoops.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinkingProblems
+{
+    class Loops
+    {
+        public void RunFiveLoops()
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void RunNineloopsBackwards()
+        {
+            for (int i = 9; i >= 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public void UserInputLoop()
+        {
+            Console.WriteLine("How many times would you like to loop?");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i < userInput; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+        public void LoopString()
+        {
+            string word = "Hello World";
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        public void PrintCharInString()
+        {
+            string word = "Hello World";
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.WriteLine(word[i]);
+            }
+        }
+        public void PrintEverOtherCharInString()
+        {
+            string word = "Hello World";
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                  Console.WriteLine(word[i]);
+                }
+            }
+        }
+        public void PrintCharDivBy3InString()
+        {
+            string word = "Hello World";
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (i % 3 == 0)
+                {
+                  Console.WriteLine(word[i]);
+                }
+            }
+        }
+        public void ForEachPrintCharInString()
+        {
+            string word = "Hello World";
+            foreach (char letter in word)
+            {
+                Console.WriteLine(letter);
+            }
+        }
+    }
+}
---
+++ CriticalThinkingProblems/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CriticalThinkingProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForLoops loops = new ForLoops();
+            //loops.RunFiveLoops();
+            //loops.RunNineloopsBackwards();
+            //loops.UserInputLoop();
+            //loops.LoopString();
+            //loops.PrintCharInString();
+            //loops.ForEachPrintCharInString();
+            //loops.PrintEverOtherCharInString();
+            loops.PrintCharDivBy3InString();
+            Console.ReadLine();
+        }
+    }
+}
